80c31e7f071f83708c1917610b97283f0667264d
Joe Zufelt <joe.zufelt@gmail.com>
2019-05-09 18:53:19 -0400

    We can dance if we want to...

---
+++ client/src/components/FindStore/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./findStore.css";
+
+class FindStore extends Component {
+    render() {
+        return (
+            <div className="background">
+                This is a placeholder
+            </div>
+        );
+    }
+}
+
+export default FindStore;
--- client/src/components/SignUp/index.js
+++ client/src/components/SignUp/index.js
@@ -4,22 +4,25 @@ import "./signup.css";
 class SignUp extends Component {
     render() {
         return (
-            <div>
-                <form>
-                    <div className="form-group">
-                        <label for="email">Email Address</label>
-                        <input type="email" className="form-control email" placeholder="What is your email?"></input>
-                    </div>
-                    <div className="form-group">
-                        <label for="password">Password</label>
-                        <input type="password" className="form-control password" placeholder="What's the password?"></input>
-                    </div>
-                    <div className="form-group">
-                        <label for="username">Username</label>
-                        <input type="text" className="form-control username" placeholder="And be a little creative..."></input>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </form>
+            <div className="background">
+                {/* <img className="loginScroll" src={Scroll} alt="scroll" /> */}
+                <div className="form">
+                    <form>
+                        <div className="form-group">
+                            <label for="email">Email Address</label>
+                            <input type="email" className="form-control email input" placeholder="What is your email?"></input>
+                        </div>
+                        <div className="form-group">
+                            <label for="password">Password</label>
+                            <input type="password" className="form-control password input" placeholder="What's the password?"></input>
+                        </div>
+                        <div className="form-group">
+                            <label for="password">Username</label>
+                            <input type="password" className="form-control password input" placeholder="And be clever"></input>
+                        </div>
+                        <button type="submit" class="btn loginSubmit">Submit</button>
+                    </form>
+                </div>
             </div>
         );
     }
