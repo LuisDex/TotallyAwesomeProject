afffed62c5bbe63a5cff82a10e3d207b19aae149
Joe Zufelt <joe.zufelt@gmail.com>
2019-05-09 15:20:37 -0400

    adding more things

---
+++ client/src/components/Home/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./home.css";
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                This is the Home Page
+            </div>
+        );
+    }
+}
+
+export default Home;
