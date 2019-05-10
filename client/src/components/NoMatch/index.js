bafb5f438facd854f0fe975e2fa91cb2e70da55b
Joe Zufelt <joe.zufelt@gmail.com>
2019-05-09 21:34:00 -0400

    So we've been told and some choose to believe it...

--- client/src/components/NoMatch/index.js
+++ client/src/components/NoMatch/index.js
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
 import "./nomatch.css";
+import Header from "../Header";
+import Footer from "../Footer";
+import Orc from "./orc.png";
+import Tavern from "./tavern.png";
 
 class NoMatch extends Component {
     render() {
         return (
-            <div>
-                404
+            <div className="bigContainer">
+                <div classNAme="wrapper">
+                    <Header />
+                    <img className="tavern" src={Tavern} />
+                    <img className="orc" src={Orc} />
+                    <p className="fourOhFour">You've made a wrong turn...</p>
+                    <Footer />
+                </div>
             </div>
         );
     }
