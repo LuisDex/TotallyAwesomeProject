788954c2827898caa413a1f08349d6c14d7b043a
Joe Zufelt <joe.zufelt@gmail.com>
2019-05-10 11:02:16 -0400

    Find Guild form needs to be updated

--- client/src/pages/findGuild.js
+++ client/src/pages/findGuild.js
@@ -1,15 +1,11 @@
 import React, { Component } from "react";
 import FindGuild from "../components/FindGuild";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
 
 class FindGuildPage extends Component {
     render() {
         return (
             <div>
-                <Header />
                 <FindGuild />
-                <Footer />
             </div>
         );
     }
