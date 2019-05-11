a725da32fad5c65e9bdd7e5e278c77d038f910b5
Joe Zufelt <joe.zufelt@gmail.com>
2019-05-10 22:12:49 -0400

    last minute jams

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -8,14 +8,17 @@ class Home extends Component {
     render() {
         return (
             <div className="bigContainer">
-            <div className="wrapper">
                 <Header />
-                <img className="wumbotron" src={Wumbo} />
-                <h1 className="wumboTitle">HAIL AND WELL MET, TRAVELER!</h1>
-                <p className="wumboText">Come on in and find your new guild!</p>
-                <a href="/findGuild"><button className="btn wumboButton">Find Guild</button></a>
-                <Footer />
+                <div className="wrapper">
+                    <img className="wumbotron" src={Wumbo} />
+                    <h1 className="wumboTitle">HAIL AND WELL MET, TRAVELER!</h1>
+                    <p className="wumboText">Come on in and find your new guild!</p>
+                    <a href="/findGuild"><button className="btn wumboButton">Find Guild</button></a>
+                    <div className="content">
+                        
+                    </div>
                 </div>
+                <Footer />
             </div>
         );
     }
